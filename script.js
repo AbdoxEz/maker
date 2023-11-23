6652219bbdcec88637a231c86c344e54f3611d73
Abdelaali Ezmamti <116950215+AbdoxEz@users.noreply.github.com>
2023-11-23 08:40:12 +0000

    Update script.js

--- script.js
+++ script.js
@@ -16,6 +16,15 @@ const setError = (element, message) => {
     inputControl.classList.remove('success')
 }
 
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
 const isValidEmail = email => {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
@@ -23,9 +32,11 @@ const isValidEmail = email => {
 
 const validateInputs = () =>{
     const emailValue = email.value.trim();
-     if(emailValue === '') {
+   if(emailValue === '') {
         setError(email, 'Email is required');
     } else if (!isValidEmail(emailValue)) {
-        setError(email, 'Oops! That doesn’t look like an email address');
-    } 
+        setError(email, 'Provide a valid email address');
+    } else {
+        setSuccess(email);
+    }
 };
